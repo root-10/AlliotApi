a7b90580c5e7ae363a4d32034c725e414c4649c9
Luis Miranda <miranda.geldres@gmail.com>
2021-07-23 10:46:44 -0400

    login, se quita pass por seguridad

--- api/auth/handler.js
+++ api/auth/handler.js
@@ -5,9 +5,11 @@ module.exports = {
   async postLogin(req, res) {
     if (!req.body.user || !req.body.pass) return res.sendStatus(400);
     try {
-      const user = await _datastore.postLogin(req.body.user, req.body.pass);
+      let user = await _datastore.postLogin(req.body.user, req.body.pass);
       if (user) {
-        return res.send({ ...user, login_date: _utils.getCurrentDate(), login_timestamp: _utils.getTimestamp() });
+        let u = JSON.parse(JSON.stringify(user));
+        delete u.pass;
+        return res.send({ ...u, login_date: _utils.getCurrentDate(), login_timestamp: _utils.getTimestamp() });
       } else {
         return res.sendStatus(400);
       }
